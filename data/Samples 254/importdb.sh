5ab1d8f133a41bf6d02b961c97d84427639fd769
Bo Gao <kaye_gao@hotmail.com>
2017-09-06 09:52:40 +0200

    removed dataset 254

--- data/Samples 254/importdb.sh
+++
@@ -1,4 +0,0 @@
-mongoimport --db test --collection biosamples --drop --file ./biosamples.json --jsonArray
-mongoimport --db test --collection individuals --drop --file ./individuals.json --jsonArray
-mongoimport --db test --collection callsets --drop --file ./callsets.json --jsonArray
-mongoimport --db test --collection variants --drop --file ./variants.json --jsonArray
